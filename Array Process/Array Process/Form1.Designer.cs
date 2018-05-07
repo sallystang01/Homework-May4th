34ab6f90132ce7bab4b62e51fe803d475830a203
Hunter Duerr <34355801+sallystang01@users.noreply.github.com>
2018-05-07 14:19:35 -0400

    Final

---
+++ Array Process/Array Process/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Array_Process
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblAboveAverage = new System.Windows.Forms.Label();
+            this.lblBelowAverage = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(219, 182);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(127, 182);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 18;
+            this.btnCalc.Text = "Get Scores";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Number of Scores Above Average :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Number of Scores Below Average :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(216, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Average Score :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAboveAverage
+            // 
+            this.lblAboveAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAboveAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAboveAverage.Location = new System.Drawing.Point(327, 90);
+            this.lblAboveAverage.Name = "lblAboveAverage";
+            this.lblAboveAverage.Size = new System.Drawing.Size(100, 23);
+            this.lblAboveAverage.TabIndex = 14;
+            this.lblAboveAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBelowAverage
+            // 
+            this.lblBelowAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblBelowAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBelowAverage.Location = new System.Drawing.Point(327, 123);
+            this.lblBelowAverage.Name = "lblBelowAverage";
+            this.lblBelowAverage.Size = new System.Drawing.Size(100, 23);
+            this.lblBelowAverage.TabIndex = 13;
+            this.lblBelowAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(327, 58);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(100, 23);
+            this.lblAverage.TabIndex = 12;
+            this.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(80, 132);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 227);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblAboveAverage);
+            this.Controls.Add(this.lblBelowAverage);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Test Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblAboveAverage;
+        private System.Windows.Forms.Label lblBelowAverage;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Array Process/Array Process/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Array_Process
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ReadScores(List<int> scoresList)
+        {
+            try
+            {
+                // Open the TestScores.text file.
+                StreamReader inputFile = File.OpenText("TestScores.txt");
+
+                // Read the scores into the list
+
+                while (!inputFile.EndOfStream)
+                {
+                    scoresList.Add(int.Parse(inputFile.ReadLine()));
+                }
+
+                // Close the file
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // The DisplayScores method displays the contents of the 
+        // scoresList parameter in the ListBox control
+        private void DisplayScores(List<int> scoresList)
+        {
+            foreach (int Score in scoresList)
+            {
+                listBox1.Items.Add(Score);
+            }
+        }
+
+        // The Average method returns the average of the values
+        // in the parameter
+        private double Average(List<int> scoresList)
+        {
+            int counter = 0; // Counter variable 
+            double average; // to hold the average
+
+            // Calculate the toal of the socres
+            foreach (int score in scoresList)
+            {
+                counter += score;
+            }
+
+            // Calculate the average of the scores
+            average = (double)counter / scoresList.Count;
+
+            // return the average
+            return average;
+        }
+
+        // The AvoveAverage method returns the number of
+        // abover average scores in the scores list
+        private int AboveAverage(List<int> scoresList)
+        {
+            int numAbove = 0;       // Counter
+
+            // Get the average score.
+            double avg = Average(scoresList);
+
+            // Count the number of above average scores
+            foreach (int score in scoresList)
+            {
+                if (score > avg)
+                {
+                    numAbove++;
+                }
+            }
+            // Return the numberof above average scores
+            return numAbove;
+        }
+
+        // Below average method returns the number of 
+        // below average scores in socresList
+        private int BelowAverage(List<int> scoresList)
+        {
+            int numBelow = 0; // counter
+
+            // Get the average score
+            double avg = Average(scoresList);
+
+            // Count the number of below average scores
+            foreach (int score in scoresList)
+            {
+                if (score < avg)
+                {
+                    numBelow++;
+                }
+            }
+            // Return the numbre of below average scores
+            return numBelow;
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+
+            double averageScore;        // To hold the average score
+            int numAboveAverage;        // number of above average scores
+            int numBelowAverage;        // Numbr of below average scores
+
+            // Create a list to hold the scores
+            List<int> scoresList = new List<int>();
+
+            // Read the scores from the filed into the list
+            ReadScores(scoresList);
+
+            // Display the scores
+            DisplayScores(scoresList);
+
+            // Display the average score
+            averageScore = Average(scoresList);
+            lblAverage.Text = averageScore.ToString();
+
+            // Display the number of above average scores 
+            numAboveAverage = AboveAverage(scoresList);
+            lblAboveAverage.Text = numAboveAverage.ToString();
+
+            // Display the number of below average scores
+            numBelowAverage = BelowAverage((scoresList));
+            lblBelowAverage.Text = numBelowAverage.ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Closes applciation
+            Application.Exit();
+        }
+    }
+}
+
